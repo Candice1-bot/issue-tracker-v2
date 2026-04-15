6e7a9597c768a7f74c302fe371af114b2984d9ee
Candice <candicejiang1@gmail.com>
2026-04-15 11:54:24 +0100

    debug home page empty

--- app/page.tsx
+++ app/page.tsx
@@ -18,6 +18,10 @@ export default async function Home() {
   return (
     <Grid columns={{ initial: "1", md: "2" }} gap="5">
       <Flex direction="column" gap="5">
+        <p>{open}</p>
+        <p>{inProgress}</p>
+        <p>{closed}</p>
+
         <IssueSummary open={open} inProgress={inProgress} closed={closed} />
         <IssueChart open={open} inProgress={inProgress} closed={closed} />
       </Flex>
